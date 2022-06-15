bc3e8611867c57dcb4013fb52017e4f29bb013ce
Tristan Elliott <>
2022-06-14 22:38:46 -0300

    adding custom RecyclerView

--- app/src/main/java/com/elliottsoftware/ecalvingtracker/customViews/CustomCardView.kt
+++ app/src/main/java/com/elliottsoftware/ecalvingtracker/customViews/CustomCardView.kt
@@ -1,4 +1,24 @@
 package com.elliottsoftware.ecalvingtracker.customViews
 
-class CustomCardView {
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.RelativeLayout
+import androidx.cardview.widget.CardView
+import androidx.core.view.isVisible
+
+class CustomCardView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int =0
+) : CardView(context,attrs,defStyleAttr) {
+    init {
+        isClickable = true
+    }
+
+    override fun performClick(): Boolean {
+        if (super.performClick()) return true
+        this.getChildAt(2).isVisible = false
+        isVisible = false
+        return true
+    }
 }
